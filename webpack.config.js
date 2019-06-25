cc351bb10eb8b0b4cdd5a43a5ab68d8ac006c0f5
Karl Hobley <karl@kaed.uk>
2019-06-25 20:14:13 +0100

    Exclude React and ReactDOM from bundle (properly this time)

--- webpack.config.js
+++ webpack.config.js
@@ -34,10 +34,25 @@ module.exports = {
   },
   externals: {
     annotator: 'annotator',
-    'react': 'React',
-    'react-dom': 'ReactDOM',
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'ReactDOM',
+        root: 'ReactDOM'
+    }
   },
   resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
